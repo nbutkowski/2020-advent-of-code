e177266f6ffa40033aec6303b0ca511af5fe6261
Nicholas Butkowski <NButkowski@commercehub.com>
2020-12-06 13:03:40 -0500

    06a: refactor input file reading

--- src/05a/index.js
+++ src/05a/index.js
@@ -6,7 +6,6 @@ const file = process.argv[2];
 
 if (file) {
     const result = readInputFile(file, '\n').reduce((max, datum) => {
-        console.log(`${typeof max} ${max}`);
         const boardingPass = deserializeBarcode(datum);
         return (boardingPass.seatId > max) ? boardingPass.seatId : max;
     }, 0);
